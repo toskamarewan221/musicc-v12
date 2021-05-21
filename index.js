b2b0d1f21d10db5840d3ec6d34fa571f094269b5
toskamarewan221 <84507794+toskamarewan221@users.noreply.github.com>
2021-05-21 03:05:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('');
+client.login('ODQ1MDg5MDY4NTY4MTUwMDI3.YKb4hg.w6M7BFptZnNNa0-aemRPO-iI8Sg');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
